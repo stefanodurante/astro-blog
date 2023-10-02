2a05ec29adceabdcd1cebb8380b7ce253aafea5c
sdurante <sdurante@leadtech.com>
2023-10-02 09:27:26 +0200

    Final push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://aesthetic-centaur-861f29.netlify.app/",
+  integrations: [preact()]
+});
